6b163f7b05c4013c8d665b5e38c0ab506f137a1a
codexmish <majharulislam.shanto.70@gmail.com>
2026-05-30 16:28:36 +0600

    cors setup done on server

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,10 @@ import express, { type Request, type Response } from "express"
 import dns from "dns"
 import router from "./router";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 const app = express()
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
