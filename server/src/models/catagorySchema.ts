fb62155fc400060cae0cd287bb99230155a35c65
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-05 10:37:52 +0600

    catagory schema added

---
+++ server/src/models/catagorySchema.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const catagory = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnaile: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const catagorySchema = mongoose.model("catagory", catagory);
